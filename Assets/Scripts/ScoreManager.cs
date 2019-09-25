6ac0f098e0c40472bc21c742208d9cd39d71a012
kyarnoff <55671137+kyarnoff@users.noreply.github.com>
2019-09-25 11:30:32 -0500

    Score
    Added ScoreManager and Obstacle scripts, which will update the player's score whenever an obstacle collides with another object.

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public int p1Score;
+    //public int p2Score;
+
+    public Text scoreText1;
+    //public Text scoreText2;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        p1Score = 0;
+        //p2Score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateScore()
+    {
+        scoreText1.text = "Player 1: " + p1Score;
+        //scoreText2.text = "Player 2: " + p2Score;
+    }
+
+}
