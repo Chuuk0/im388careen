aaac3cbb36c85db2a1f27a2570580e08d1e24f60
Jhyland-r <jakehyland001@gmail.com>
2019-09-27 05:50:04 -0500

    Test Level (2 Player Oriented)
    -Created Level with two players in mind, filled with physics blocks, mines, and static obstacles to traverse and hide around.
    -Added jump script to Tank using spacebar, exit functionality once built out with esc, and resetting the current scene with the r-key.
    -Increased Tank speed considering level size and obstacle interaction.

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Jump : MonoBehaviour
+{
+    public Rigidbody rb;
+    private bool JumpTimer = true;
+    public float JumpUp = 4500;
+    public float JumpForward = 3000;
+    public float JumpWait = 3.2F;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space") && JumpTimer == true)
+        {
+            print("jump");
+            JumpTimer = false;
+            //rb.AddExplosionForce(800, new Vector3(transform.position.x, transform.position.y, transform.position.z - 0.5f), 10, 300, ForceMode.Force);
+            rb.AddForce(JumpForward, JumpUp, 0, ForceMode.Impulse);
+            StartCoroutine(Jumping());
+        }
+
+        if (Input.GetKey(KeyCode.R))
+        {
+            int scene = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(scene, LoadSceneMode.Single);
+            Time.timeScale = 1;
+        }
+    }
+
+    IEnumerator Jumping()
+    {
+        yield return new WaitForSeconds(JumpWait);
+        JumpTimer = true;
+    }
+}
